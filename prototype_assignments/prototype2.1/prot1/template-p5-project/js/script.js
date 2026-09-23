987333c29782b3e6a6cfa1ced143581c260be449
charlestgobeil <charles.tgobeil@gmail.com>
2026-09-23 09:45:34 -0400

    prototype 2.1 started

---
+++ prototype_assignments/prototype2.1/prot1/template-p5-project/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(500, 500);
+    background(20, 70, 255);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+    fill(255, 0, 0);
+    circle(250, 250, 100)
+
+    fill(69, 100, 10);
+    rect(50, 200, 100, 200);
+
+    fill(250, 200, 100);
+    rect(400, 450, 200, 25);
+
+    fill(133, 0, 250);
+    rect(250, 20, 30, 300);
+
+    fill(10, 150, 80);
+    rect(40, 40, 400, 80);
+
+    fill(70, 20, 40);
+    rect(300, 400, 150, 100);
+
+    fill(180, 160, 10);
+    rect(200, 380, 300, 10);
+
+
+
+}
